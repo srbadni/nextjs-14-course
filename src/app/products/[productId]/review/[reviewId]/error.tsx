e6e41947305003d09d6333dd123218ec734fd1b7
Alireza <tafreshi13@gmail.com>
2024-07-11 19:51:49 +0330

    error.tsx special file in next js

---
+++ src/app/products/[productId]/review/[reviewId]/error.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react';
+
+const ErrorBoundary = () => {
+    return (
+        <div>
+            در بارگزاری نظر مشکلی رخ داده است
+        </div>
+    );
+};
+
+export default ErrorBoundary;
--- src/app/products/[productId]/review/[reviewId]/page.tsx
+++ src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import {notFound} from "next/navigation";
 
+const getRandomInt = (count: number) => {
+    return Math.floor(Math.random() * count);
+}
+
 const ReviewItem = ({params}: {
     params: {
         productId: string,
         reviewId: string,
     }
 }) => {
+    if (getRandomInt(2) === 1) {
+        throw new Error("خطا در بارگزاری");
+    }
     if (parseInt(params.reviewId) > 1000) {
         notFound();
     }
