02b3ea8c1317a2d1c1ff60be415d50ee19eb11dd
Alireza <tafreshi13@gmail.com>
2024-10-17 23:10:03 +0330

    handling PATCH request

--- route-handlers/src/app/comments/[id]/route.ts
+++ route-handlers/src/app/comments/[id]/route.ts
@@ -4,3 +4,11 @@ export async function GET(_request: Request, {params}: {params: { id: string }})
     const comment = comments.find(comment => comment.id === params.id);
     return Response.json(comment);
 }
+
+export async function PATCH(request: Request, {params}: {params: { id: string }}) {
+    const body = await request.json();
+    const {text} = body;
+    const index = comments.findIndex(comment => comment.id === params.id);
+    comments[index].text = text;
+    return Response.json(comments[index]);
+}
