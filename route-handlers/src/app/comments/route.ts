29aa1758c32d10e88132faabe45675f8906ec831
Alireza <tafreshi13@gmail.com>
2024-10-17 19:25:26 +0330

    httpPOST request

--- route-handlers/src/app/comments/route.ts
+++ route-handlers/src/app/comments/route.ts
@@ -3,3 +3,18 @@ import {comments} from "@/app/comments/data";
 export async function GET() {
     return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+    const comment = await request.json();
+    const newComment = {
+        id: String(comments.length + 1),
+        text: comment.text
+    }
+    comments.push(newComment);
+    return new Response(JSON.stringify(newComment), {
+        status: 201,
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+}
