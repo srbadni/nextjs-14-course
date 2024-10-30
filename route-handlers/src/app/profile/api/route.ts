e7b145a49104f7440db387c7c59244dda7237656
Alireza <tafreshi13@gmail.com>
2024-10-30 16:03:43 +0330

    cookies in route handlers

--- route-handlers/src/app/profile/api/route.ts
+++ route-handlers/src/app/profile/api/route.ts
@@ -1,14 +1,18 @@
 import {NextRequest} from "next/server";
-import {headers} from "next/headers";
+import {headers, cookies} from "next/headers";
 
 export async function GET(request: NextRequest) {
     const myHeaders = new Headers(request.headers);
     const headersList = headers();
+    cookies().set("age", "20");
     console.log(myHeaders.get("Authorization"));
     console.log(headersList.get("Authorization"));
+    console.log(request.cookies.get("theme"), "theme");
+    console.log(cookies().get("age"), "age")
     return new Response("<h1>api پروفایل</h1>", {
         headers: {
             "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
         }
     });
 }
