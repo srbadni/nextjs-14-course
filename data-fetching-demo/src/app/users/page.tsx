d64bde1260e196ef9aa4fdf9c6c0e420369b5efe
Alireza <tafreshi13@gmail.com>
2024-12-13 18:32:38 +0330

    Add UsersPage component for fetching and displaying user data

---
+++ data-fetching-demo/src/app/users/page.tsx
@@ -0,0 +1,23 @@
+type User = {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+}
+
+export default async function UsersPage() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json() as User[];
+    console.log(users, "users");
+    
+    return <ul>
+        {
+            users.map(user => {
+                return <li key={user.id}>
+                    {user.name} - {user.username} - {user.phone} - {user.email}
+                </li>
+            })
+        }
+    </ul>
+}
